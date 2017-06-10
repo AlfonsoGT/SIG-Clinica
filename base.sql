a5def1da483314334e25e0fbaa338f520ddf6750
yaneth94 <marvinalvarenga96@gmail.com>
2017-06-10 16:56:22 -0300

    Base que contiene la informacion de facultades

---
+++ base.sql
@@ -0,0 +1,17 @@
+INSERT INTO procedencia (nombre_procedencia) VALUES
+	('Facultad de Medicina'),
+	('Facultad de Odontologia'),
+	('Facultad de Ciencias Económicas'),
+	('Facultad de Ciencias Naturales y Matemática'),
+	('Facultad de Ciencias Naturales y Humanidades'),
+	('Facultad de Ciencias Agronómicas'),
+	('Facultad de Ingeniería y Arquitectura'),
+	('Facultad de Jurisprudencia y Ciencias Sociales'),
+	('Facultad de Quimica y Farmacia'),
+	('Facultad Multidisciplinaria de Occidente'),
+	('Facultad Multidisciplinaria Oriental'),
+	('Facultad Multidisciplinaria Paracentral');
+
+INSERT INTO sexo (nombre_sexo) VALUES
+	('Femenino'),
+	('Masculino');
