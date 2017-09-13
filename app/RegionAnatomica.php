e4ddadc8fbe68adafb49fc21ce09575f585fada4
alegranados03 <alegranados03@gmail.com>
2017-09-12 20:34:59 -0600

    arreglando una R :V

--- app/RegionAnatomica.php
+++ app/RegionAnatomica.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class RegionAnatomica extends Model
 {
-     protected $table = 'RegionAnatomica';
+     protected $table = 'regionAnatomica';
     protected $primaryKey = 'idRegionAnatomica';
 }
