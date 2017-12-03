52c5ec3b667829f5d9b03f26e27062747e6ae7a1
alegranados03 <alegranados03@gmail.com>
2017-12-03 10:59:14 -0600

    arreglo tabla examen

--- app/Examen.php
+++ app/Examen.php
@@ -11,7 +11,7 @@ class Examen extends Model
 protected $fillable = [
    'idUsuario', 'idReservacion','fechaRealizacion',
 ];
-protected $table = 'Examen';
+protected $table = 'examen';
   protected $primaryKey = 'idExamen';
   public $timestamps = false;
 
